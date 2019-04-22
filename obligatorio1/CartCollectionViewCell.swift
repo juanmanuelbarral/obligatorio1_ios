831eff385f0f0c24e6b913adaddb288d17ee4ad6
juanmanuelbarral <manuelbarral02@gmail.com>
2019-04-22 02:25:13 -0300

    Classes created: CheckoutViewController (for the checkout scene), CartCollectionViewCell, ItemTableViewCell Launch storyboard rectified Main storyboard modified

---
+++ obligatorio1/CartCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CartCollectionViewCell.swift
+//  obligatorio1
+//
+//  Created by Manu on 21/4/19.
+//  Copyright © 2019 Manuel Barral. All rights reserved.
+//
+
+import UIKit
+
+class CartCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ obligatorio1/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  obligatorio1
+//
+//  Created by Manu on 21/4/19.
+//  Copyright © 2019 Manuel Barral. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
