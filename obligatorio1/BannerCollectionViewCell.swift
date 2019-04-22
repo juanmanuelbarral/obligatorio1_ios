876b200d48b05a37d6f0aac75a403e228ba2d85b
juanmanuelbarral <manuelbarral02@gmail.com>
2019-04-22 02:22:58 -0300

    Assets loaded into the project

---
+++ obligatorio1/BannerCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BannerCollectionViewCell.swift
+//  obligatorio1
+//
+//  Created by Manu on 21/4/19.
+//  Copyright © 2019 Manuel Barral. All rights reserved.
+//
+
+import UIKit
+
+class BannerCollectionViewCell: UICollectionViewCell {
+    
+}
