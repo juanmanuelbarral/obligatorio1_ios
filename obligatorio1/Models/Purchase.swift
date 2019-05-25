6f98968ecaecc86a4388740706660232510d70b7
juanmanuelbarral <manuelbarral02@gmail.com>
2019-05-25 17:20:33 -0300

    Purchases calculate total price

--- obligatorio1/Models/Purchase.swift
+++ obligatorio1/Models/Purchase.swift
@@ -24,15 +24,16 @@ class Purchase {
     required init?(map: Map) {
         if map.JSON[Constants.Purchase.DATE_KEY] == nil { return nil }
         if map.JSON[Constants.Purchase.PRODUCTS_KEY] == nil { return nil }
-        self.total = calculateTotal()
     }
     
     private func calculateTotal() -> Float {
         var totalPrice: Float = 0
         guard let products = products else { return totalPrice }
         products.forEach { (item: CheckoutItem) in
-            // I know price is not empry bc in the init? of Product if price==nil -> doesn't create that Product
-            totalPrice += Float(item.quantity) * item.product!.price!
+            if let product = item.product {
+                let price = product.price ?? 0
+                totalPrice += Float(item.quantity) * price
+            }
         }
         return totalPrice
     }
@@ -43,5 +44,6 @@ extension Purchase: Mappable {
     func mapping(map: Map) {
         date <- map[Constants.Purchase.DATE_KEY]
         products <- map[Constants.Purchase.PRODUCTS_KEY]
+        total = calculateTotal()
     }
 }
